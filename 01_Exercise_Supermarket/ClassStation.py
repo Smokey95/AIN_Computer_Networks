e8f5d194ee16efc7059ab9acdffc7353ee9e5866
Smokey95 <simon.htwg@gmail.com>
2022-10-13 17:43:40 +0200

    Testing some nice stuff

--- 01_Exercise_Supermarket/ClassStation.py
+++ 01_Exercise_Supermarket/ClassStation.py
@@ -6,9 +6,16 @@
 class Station():
   # please implement here
   
-  def __init__(self, operatingTime, name):
-    self.operatingTime = operatingTime
+  def __init__(self, delay_per_item, name):
     self.name = name
+    self.buffer = []
+    self.delay_per_item = delay_per_item
+    self.CustomerWaiting = False
+    self.busy = False
     
-  def __str__(self) -> str:
+  def lineUp(self, customer):
+    self.buffer.append(customer)
+    self.CustomerWaiting = True
+  
+  def __str__(self):
     return self.name
