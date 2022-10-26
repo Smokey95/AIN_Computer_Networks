2067f93e90669ef462c0f80b8ac799ced08bc804
lars buerger <larsbuerger@gmail.com>
2022-10-26 16:14:45 +0200

    peeeenis

--- 01_Exercise_Supermarket/2_Thread_Based/ClassCustomer.py
+++ 01_Exercise_Supermarket/2_Thread_Based/ClassCustomer.py
@@ -1,7 +1,8 @@
-from threading import Thread
+from threading import Thread, Event
 import time
 
 class Customer(Thread):
+  
   served = dict()
   dropped = dict()
   complete = 0
@@ -9,6 +10,24 @@ class Customer(Thread):
   duration_cond_complete = 0
   count = 0
 
-  def __init__():
+  def __init__(self, einkaufsliste, name, start_time):
+    self.einkaufsliste = einkaufsliste
+    self.name = name
+    self.start_time = start_time
+    print("Customer %s created" % self.name)
+    self.serveEv = Event()
     Thread.__init__(self)
+
+  def run(self):
+
+    time.sleep(self.start_time)
+
+    while(True):
+      if(self.serveEv.is_set()):
+        self.serveEv.clear()
+        self.serve()
+        break
+    print("Customer started")
+
+
 # please implement here
--- 01_Exercise_Supermarket/2_Thread_Based/ClassStation.py
+++ 01_Exercise_Supermarket/2_Thread_Based/ClassStation.py
@@ -1,4 +1,4 @@
-from threading import Thread
+from threading import Thread, Event
 import time
 
 # class consists of
@@ -7,14 +7,25 @@ import time
 # delay_per_item: service time
 # CustomerWaiting, busy: possible states of this station
 class Station(Thread):
-    def __init__(self, name, buffer, delay_per_item):
-      print("Station created")
+    
+    def __init__(self, name, delay_per_item):
+      print("Station %s created" % name)
       Thread.__init__(self)
-      #self.name = name
-      #self.buffer = buffer
-      #self.delay_per_item = delay_per_item
-      ##self.CustomerWaiting = False
-      #self.busy = False
+      self.name = name
+      self.buffer = []
+      self.delay_per_item = delay_per_item
+      self.CustomerWaitingEv = Event()
+      self.serveEv = None
+      self.busy = False
     
     def run(self):
-      print("Station started")
+      print("Station %s waiting for customers" % self.name)
+      while True:
+        self.CustomerWaitingEv.wait()
+
+        if(self.CustomerWaitingEv.is_set()):
+          self.busy = True
+          self.CustomerWaitingEv.clear()
+          self.serveEv = Event((10, self.kundenliste[0]))
+          self.buffer.append(Event())
+
