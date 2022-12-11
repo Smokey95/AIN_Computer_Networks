e9618bd8034ab71bf4025e1dc0ac1d0fe0dfc2db
Smokey95 <simon.htwg@gmail.com>
2022-12-11 17:30:59 +0100

    Update

--- 04_Exercise_Sockets/4_5/port_scanner.py
+++ 04_Exercise_Sockets/4_5/port_scanner.py
@@ -3,26 +3,41 @@ from threading import Thread
 import time
 
 Server_IP = '141.37.168.26'
-Server_PORT = 500000
 MESSAGE = 'hello'
 
 def scan_port(port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(100)
-    print('Connecting to TCP server with IP ', Server_IP, ' on Port ', port)
-    sock.connect((Server_IP, port))
-    print('Sending message', MESSAGE)
-    sock.send(MESSAGE.encode('utf-8'))
+    
+    tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_client.settimeout(10)
+    
     try:
-        msg=sock.recv(1024).decode('utf-8')
-        print('Message received; ', msg)
+        print('Connecting to TCP server with IP ', Server_IP, ' on Port ', port)
+        #tcp_client.connect((Server_IP, port))
+        result = tcp_client.connect_ex((Server_IP, port))
+        
+        if result == 0:
+            print('Connection established at',time.asctime(), 'on port', port)
+        elif result == 10061:
+            print('Connection refused [WinEr: 10061] at',time.asctime() , 'on port', port)
+        elif result == 10060:
+            print('Connection timed out at',time.asctime(), "on port", port)
+        elif result == 10051:
+            print('Network is unreachable at',time.asctime(), "on port", port)
+        else:
+            print('Unknown error at',time.asctime(), "on port", port)
+        
     except socket.timeout:
-        print('Socket timed out at',time.asctime())
-    sock.close()
+        print("Connection timed out at",time.asctime(), "on port", port)
+    except ConnectionRefusedError:
+        print('Connection refused [WinEr: 10061] at',time.asctime() , 'on port', port)
+    except socket.error:
+        print('Socket error at',time.asctime(), 'on port', port)
+    finally:
+        tcp_client.close()
 
 if __name__ == '__main__':
-    # for i in range(1, 50):
-    #     t=Thread(target=scan_port, args=(i,))
-    #     t.start()
-    scan_port(7)
+     for i in range(1, 51):
+         t=Thread(target=scan_port, args=(i,))
+         t.start()
+    #scan_port(7)
 
