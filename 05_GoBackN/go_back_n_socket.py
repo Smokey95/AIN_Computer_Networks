7084eabccbc04a931386efd20e55fd9337f61378
Smokey95 <simon.htwg@gmail.com>
2023-01-04 13:33:56 +0100

    First version, send recv is possible, go-back-n not implemented yet

--- 05_GoBackN/go_back_n_socket.py
+++ 05_GoBackN/go_back_n_socket.py
@@ -1,9 +1,22 @@
 import socket
 from threading import Thread
 import random
-#import lossy_udp_socket
+
+from lossy_udp_socket import lossy_udp_socket
 
 class go_back_n_socket():
   
-  def __init__(self) -> None:
-    pass
+  def __init__(self, localPort, remotePort, remoteAddress):
+    self.localPort = localPort
+    self.remotePort = remotePort
+    self.remoteAddress = remoteAddress
+    self.sock = lossy_udp_socket(self, localPort, (remoteAddress, remotePort), 1)
+
+  def send(self, msg):
+    print('[Socket] Sending message: ', msg)
+    self.sock.send(msg.encode('utf-8'))
+    
+  def receive(self, packet):
+    msg = packet.decode('utf-8')
+    print('[Socket] Received message: ', msg)
+    return msg
--- 05_GoBackN/main.py
+++ 05_GoBackN/main.py
@@ -1,6 +1,35 @@
 import socket
 from threading import Thread
 import random
+from time import sleep
+
+from go_back_n_socket import go_back_n_socket
 
 if __name__ == '__main__':
-  pass
+  
+  print('MAIN INFO | Starting main.py')
+  sleep(0.1)
+  
+  # Create a server socket
+  # Server Port:    12000
+  # Remote Port:    12001
+  # Remote Address: localhost
+  # Server should be listening after creation (see lossy_udp_socket.py Thread initialization)
+  serverSocket = go_back_n_socket(12000, 12001, '127.0.0.1')
+  
+  # Create a client socket
+  # Local Port:     12001
+  # Remote Port:    12000
+  # Remote Address: localhost
+  clientSocket = go_back_n_socket(12001, 12000, '127.0.0.69')
+  
+  # Send msg to server
+  clientSocket.send('Hello World')
+  
+  # Wait for server to receive msg
+  sleep(1)
+  
+  # Terminate sockets
+  print('MAIN INFO | Terminating sockets')
+  serverSocket.sock.stop()
+  clientSocket.sock.stop()
