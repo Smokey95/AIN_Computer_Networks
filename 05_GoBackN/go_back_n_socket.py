278be952626cc5c68f0fbd26a4aa63dcf7cf5f00
Smokey95 <simon.htwg@gmail.com>
2023-01-04 14:52:00 +0100

    Implement Argument Parser and testfile

--- 05_GoBackN/go_back_n_socket.py
+++ 05_GoBackN/go_back_n_socket.py
@@ -1,22 +1,43 @@
 import socket
 from threading import Thread
 import random
-
+from math import ceil
 from lossy_udp_socket import lossy_udp_socket
+
 
 class go_back_n_socket():
   
-  def __init__(self, localPort, remotePort, remoteAddress):
+  def __init__(self, localPort, remotePort, remoteAddress, PLR=0.1, segmentSize=1000, windowSize=10, debugFlag=False):
     self.localPort = localPort
     self.remotePort = remotePort
     self.remoteAddress = remoteAddress
-    self.sock = lossy_udp_socket(self, localPort, (remoteAddress, remotePort), 1)
+    self.sock = lossy_udp_socket(self, localPort, (remoteAddress, remotePort), PLR)
+    self.segmentSize = segmentSize
+    self.windowSize = windowSize
+    self.debugFlag = debugFlag
 
   def send(self, msg):
-    print('[Socket] Sending message: ', msg)
-    self.sock.send(msg.encode('utf-8'))
+    # Get the length of the message
+    msgLen = len(msg)
+    print('SOCK DEBUG | Message length: ', msgLen) if self.debugFlag else None
+    
+    # Get the number of segments
+    numSegments = ceil(msgLen / self.segmentSize)
+    print('SOCK DEBUG | Number of segments: ', numSegments) if self.debugFlag else None
+    
+    # Store the message in a list of segments
+    msgSegments = []
+    for i in range(numSegments):
+      msgSegments.append(msg[i * self.segmentSize : (i + 1) * self.segmentSize])
+      print('SOCK DEBUG | Message segments: ', msgSegments) if self.debugFlag else None
+    
+    # Send the message segments
+    for i in range(numSegments):
+      print('SOCK DEBUG | Sending segment: ', msgSegments[i]) if self.debugFlag else None
+      self.sock.send(msgSegments[i].encode('utf-8'))
+  
     
   def receive(self, packet):
     msg = packet.decode('utf-8')
-    print('[Socket] Received message: ', msg)
+    print('SOCK DEBUG | Received message: ', msg) if self.debugFlag else None
     return msg
--- 05_GoBackN/main.py
+++ 05_GoBackN/main.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------------------------- Imports
 import socket
 from threading import Thread
 import random
@@ -5,9 +6,24 @@ from time import sleep
 
 from go_back_n_socket import go_back_n_socket
 
+import argparse
+
+# -------------------------------------------------------------------------------------------------- Argument Parser
+parser = argparse.ArgumentParser(description='Go-Back-N Socket', add_help=True)
+parser.add_argument('-S', '--serverPort', type=int, default=12000, help='Server Port')
+parser.add_argument('-C', '--clientPort', type=int, default=12001, help='Remote Port')
+parser.add_argument('-A' ,'--clientAddress', type=str, default='127.0.0.96', help='Remote Client Address')
+parser.add_argument('-L', '--lossRate', type=float, default=0.0, help='Loss Rate')
+
+parser.add_argument('-d', '--debug', action='store_true', default=False, help='Debug Mode')
+
+
+# -------------------------------------------------------------------------------------------------- Main
 if __name__ == '__main__':
   
-  print('MAIN INFO | Starting main.py')
+  args = parser.parse_args()
+  
+  print('MAIN INFO  | Starting main.py')
   sleep(0.1)
   
   # Create a server socket
@@ -15,21 +31,23 @@ if __name__ == '__main__':
   # Remote Port:    12001
   # Remote Address: localhost
   # Server should be listening after creation (see lossy_udp_socket.py Thread initialization)
-  serverSocket = go_back_n_socket(12000, 12001, '127.0.0.1')
+  serverSocket = go_back_n_socket(args.serverPort , args.clientPort, '127.0.0.1', debugFlag=args.debug)
   
   # Create a client socket
   # Local Port:     12001
   # Remote Port:    12000
   # Remote Address: localhost
-  clientSocket = go_back_n_socket(12001, 12000, '127.0.0.69')
+  clientSocket = go_back_n_socket(args.clientPort, args.serverPort, args.clientAddress, debugFlag=args.debug)
   
   # Send msg to server
-  clientSocket.send('Hello World')
+  with open('testdata.txt', 'r') as f:
+    msg = f.read()
+  clientSocket.send(msg)
   
   # Wait for server to receive msg
   sleep(1)
   
   # Terminate sockets
-  print('MAIN INFO | Terminating sockets')
+  print('MAIN INFO  | Terminating sockets')
   serverSocket.sock.stop()
   clientSocket.sock.stop()
